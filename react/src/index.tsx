39e832c2a1193e8039de483ebdf2c735753df5ea
raskil <raskil@ttu.ee>
2021-05-09 14:02:09 +0300

    Added nginx conf

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -13,7 +13,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
         <React.StrictMode>
             <App />
         </React.StrictMode>,
